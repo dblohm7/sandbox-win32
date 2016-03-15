fffd4aa60fb2fa808896491248c0e5d10cc48589
Aaron Klotz <aklotz@mozilla.com>
2016-03-14 19:49:48 -0600

    Adds comtest

--- src/build.sh
+++ src/build.sh
@@ -12,3 +12,6 @@ fi
 
 cl -Zi -EHsc -MD -D_WIN32_WINNT=0x0A00 -DUNICODE -D_UNICODE -DINIT_FUNCTION_NAME=\"$INIT_FUNCTION_NAME\" -DDEINIT_FUNCTION_NAME=\"$DEINIT_FUNCTION_NAME\" proto.cpp dacl.cpp sid.cpp sidattrs.cpp WindowsSandbox.cpp advapi32.lib user32.lib rpcrt4.lib ole32.lib shlwapi.lib
 mt -manifest sandbox-win32.manifest -outputresource:proto.exe;#1
+
+cl -Zi -EHsc -MD -D_WIN32_WINNT=0x0A00 -DUNICODE -D_UNICODE comtest.cpp dacl.cpp sid.cpp sidattrs.cpp WindowsSandbox.cpp advapi32.lib user32.lib rpcrt4.lib ole32.lib shlwapi.lib
+mt -manifest sandbox-win32.manifest -outputresource:comtest.exe;#1
---
+++ src/comtest.cpp
@@ -0,0 +1,83 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+#include <windows.h>
+#include <sddl.h>
+
+#include "dacl.h"
+#include "sid.h"
+#include "WindowsSandbox.h"
+
+using std::wcout;
+using std::wcerr;
+using std::endl;
+using std::hex;
+using mozilla::WindowsSandbox;
+using mozilla::WindowsSandboxLauncher;
+
+namespace {
+
+class COMTestSandbox : public WindowsSandbox
+{
+public:
+  explicit COMTestSandbox()
+  {}
+  virtual ~COMTestSandbox()
+  {
+    Fini();
+  }
+
+protected:
+  virtual bool OnPrivInit();
+  virtual bool OnInit();
+  virtual void OnFini();
+
+private:
+};
+
+bool
+COMTestSandbox::OnPrivInit()
+{
+  return true;
+}
+
+bool
+COMTestSandbox::OnInit()
+{
+  return true;
+}
+
+void
+COMTestSandbox::OnFini()
+{
+}
+
+}
+
+int wmain(int argc, wchar_t* argv[])
+{
+  if (argc == 1) {
+    WindowsSandboxLauncher sboxLauncher;
+    sboxLauncher.Init();
+    if (!sboxLauncher.Launch(argv[0], L"")) {
+      wcerr << L"Failed to launch" << endl;
+      return EXIT_FAILURE;
+    }
+    if (!sboxLauncher.Wait(INFINITE)) {
+      return EXIT_FAILURE;
+    }
+  }
+
+  COMTestSandbox sb;
+  if (!sb.Init(argc, argv)) {
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
+
